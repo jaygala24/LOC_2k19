7733ca53dd4369d9fa7478e254df1a62c3593891
jaygala24 <jaygala24@gmail.com>
2019-03-16 15:20:33 +0530

    Added Donation Model Manager

--- src/donations/admin.py
+++ src/donations/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import Donations
+from .models import Donation, Donor
 
-# Register your models here.
 
-admin.site.register(Donations)
+admin.site.register(Donor)
+admin.site.register(Donation)
